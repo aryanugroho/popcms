5f5fc76d8e709888ab5db3c45d790ea8d0934544
John <john@eastpoint.co.uk>
2014-05-17 09:44:49 +0100

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ popcms
 ======
 
 NodeJS, MongoDB website content management system
+
+
+
+THIS IS NOT READY FOR USE.
---
+++ test/index.js
@@ -0,0 +1,11 @@
+var should = require('chai').should(),
+    popcms = require('../index'),
+    helloWorld = popcms.helloWorld;
+
+describe('#helloWorld', function() {
+  it('Nothing to test yet', function() {
+    helloWorld().should.equal("Hello, World");
+  });
+ 
+});
+ 
